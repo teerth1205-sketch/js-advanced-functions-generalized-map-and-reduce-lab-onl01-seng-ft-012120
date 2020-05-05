4deaccd893795fafea2470e58a813716c1399aee
EC2 Default User <ec2-user@ip-172-31-36-178.us-east-2.compute.internal>
2020-05-05 23:58:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+const map = function(array, fn) {
+    let result = []
+    
+    for (let i = 0; i< array.length; i++){
+        result.push(fn(array[i]))
+    }
+    return result
+}
+
+const reduce = function(array, fn, init ) {
+    let total = init
+    for (let i = 0; i < array.length; i++) {
+        if (total === undefined){
+        total = array[i]
+        }else {
+            total = fn(array[i], total)
+        }
+    }
+    return total
+}
